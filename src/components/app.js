a3d9295f02cafb6789b7dc9a4de49740ae82f779
Hong Ma <hma@thoughtworks.com>
2018-05-11 15:39:03 +0800

    fisrt commit

---
+++ src/components/app.js
@@ -0,0 +1,99 @@
+import React from "react";
+import TodosList from "./todos-list"
+import CreateTodo from "./create-todo";
+import GithubCorner from "react-github-corner";
+
+const todos = {
+    items: [],
+    lsKey: "todos",
+    populate () {
+        this.items = this.get();
+    },
+    get () {
+        try {
+            return JSON.parse(localStorage.getItem(this.lsKey)) || []
+        } catch (e) {}
+        return [];
+    },
+    save () {
+        localStorage.setItem(this.lsKey, JSON.stringify(this.items));
+    },
+    toggle (id) {
+        let todoItem = this.items[id];
+        todoItem.isCompleted = !todoItem.isCompleted;
+        this.save();
+    },
+    add (obj) {
+        this.items.push(obj);
+        this.save();
+    },
+    remove (id) {
+        this.items.splice(id, 1);
+        this.save();
+    },
+    update (id, task) {
+        let todoItem = this.items[id];
+        todoItem.task = task;
+        this.save();
+    }
+};
+
+todos.populate();
+
+
+export default class App extends React.Component {
+    constructor (props) {
+        super(props);
+        this.state = {
+            todos: todos.items
+        };
+    }
+    render () {
+        return (
+            <div>
+                <GithubCorner
+                    href="https://github.com/IonicaBizau/react-todo-app"
+                    bannerColor="#64CEAA"
+                    octoColor="#fff"
+                    width={80}
+                    height={80}
+                    direction="right"
+                />
+                <h1>TODOs</h1>
+                <CreateTodo
+                    createTask={this.createTask.bind(this)}
+                />
+                <TodosList
+                    todos={this.state.todos}
+                    toggleTask={this.toggleTask.bind(this)}
+                    editTask={this.editTask.bind(this)}
+                    deleteTask={this.deleteTask.bind(this)}
+                />
+              <p className="total-items">共有{this.state.todos.length}个</p>
+            </div>
+        );
+    }
+
+    createTask (task) {
+        task = task.trim();
+        if (!task) { return; }
+        todos.add({
+            task,
+            isCompleted: false
+        });
+        this.setState({ todos: this.state.todos });
+    }
+
+    toggleTask (taskId) {
+        todos.toggle(taskId);
+        this.setState({ todos: this.state.todos });
+    }
+    editTask (taskId, task) {
+        todos.update(taskId, task);
+        this.setState({ todos: this.state.todos });
+    }
+    deleteTask (taskId) {
+        todos.remove(taskId);
+        this.setState({ todos: this.state.todos });
+    }
+}
---
+++ src/components/todos-list.js
@@ -0,0 +1,32 @@
+import React from "react";
+import TodosListHeader from "./todos-list-header";
+import TodosListItem from "./todos-list-item";
+
+export default class TodosList extends React.Component {
+    renderItems () {
+        return this.props.todos.map((c, index) => {
+            return (
+                <TodosListItem
+                    key={index}
+                    {...c}
+                    id={index}
+                    toggleTask={this.props.toggleTask}
+                    editTask={this.props.editTask}
+                    deleteTask={this.props.deleteTask}
+                />
+            )
+        });
+    }
+    render () {
+        if (!this.props.todos.length) {
+            return <p className="tutorial">Create your first todo! :)</p>;
+        }
+        return (
+            <table>
+                <tbody>
+                    {this.renderItems()}
+                </tbody>
+            </table>
+        )
+    }
+}
